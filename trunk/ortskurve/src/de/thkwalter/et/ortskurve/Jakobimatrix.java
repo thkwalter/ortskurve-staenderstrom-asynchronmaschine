fae8d8eeac71e40b994303f0b48edf5f0de0e8d0
th.k.walter@googlemail.com <th.k.walter@googlemail.com@08437e74-564d-73a1-201a-7a4970f44d9d>
2013-06-22 21:47:10 +0000

    Die Klasse Jakobimatrix und die Testklasse JakobimatrixTest implementiert.

--- trunk/ortskurve/src/de/thkwalter/et/ortskurve/Jakobimatrix.java
+++ trunk/ortskurve/src/de/thkwalter/et/ortskurve/Jakobimatrix.java
@@ -15,21 +15,27 @@
  */
 package de.thkwalter.et.ortskurve;
 
+import java.util.logging.Logger;
+
 import org.apache.commons.math3.analysis.MultivariateMatrixFunction;
 import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
 
 /**
  * Diese Klasse repräsentiert die Jakobi-Matrix der Modellgleichungen (der Kreisgleichungen).
- *
+ * 
  * @author Th. K. Walter
  */
 public class Jakobimatrix implements MultivariateMatrixFunction
 {
-
 /**
  * Dieses Feld enthält die Messpunkte.
  */
 private Vector2D[] messpunkte;
+
+/**
+ * Der Logger dieser Klasse.
+ */
+private static Logger logger = Logger.getLogger(Jakobimatrix.class.getName());
 
 // =====================================================================================================================
 // =====================================================================================================================
@@ -37,7 +43,7 @@ private Vector2D[] messpunkte;
 /**
  * Dieser Konstruktor initialisiert das Feld der Messpunkte.
  * 
- * @param messpunkte Das Feld der Messpunkte. 
+ * @param messpunkte Das Feld der Messpunkte.
  */
 public Jakobimatrix(Vector2D[] messpunkte)
    {
@@ -50,38 +56,53 @@ public Jakobimatrix(Vector2D[] messpunkte)
 /**
  * Dieses Feld berechnet die Jakobi-Matrix der Modellgleichungen (der Kreisgleichungen).
  * 
- * @param kreisparameter Die Parameterwerte der Kreisgleichungen. Das 0-te Element ist die x-Koordinate des 
- * Kreismittelpunkts, das 1-te Element ist die y-Koordinate des Kreismittelpunkts, das 2-te Element ist der Radius des
- * Kreises.
+ * @param kreisparameter Die Parameterwerte der Kreisgleichungen. Das 0-te Element ist die x-Koordinate des
+ *        Kreismittelpunkts, das 1-te Element ist die y-Koordinate des Kreismittelpunkts, das 2-te Element ist der
+ *        Radius des Kreises.
  * 
- * @return Die Jakobi-Matrix der Modellgleichungen. Die erste Dimension des Feldes läuft über die Gleichungen, die
- * zweite Dimension über die Kreisparameter.
+ * @return Die Jakobi-Matrix der Modellgleichungen. Der erste Index des Feldes läuft über die Gleichungen, der zweite
+ *         Index über die Kreisparameter.
  * 
  * @see org.apache.commons.math3.analysis.MultivariateMatrixFunction#value(double[])
  */
 @Override
-public double[][] value(double[] kreisparameter) throws IllegalArgumentException
+public double[][] value(double[] kreisparameter)
    {
    // Der Vektor für den Mittelpunkt der Ortskurve wird erzeugt.
    Vector2D mittelpunkt = new Vector2D(kreisparameter[0], kreisparameter[1]);
-   
+
    // Das Feld für die Jakobi-Matrix wird deklariert.
-   double[][] jakobiMatrix = new double[this.messpunkte.length][3];  
-   
+   double[][] jakobiMatrix = new double[this.messpunkte.length][3];
+
    // In dieser Schleife wird die Jakobi-Matrix initialisiert.
-   double inverserAbstandMesspunktMittelpunkt = Double.NaN;
+   double abstandMesspunktMittelpunkt = Double.NaN;
    for (int i = 0; i < this.messpunkte.length; i++)
       {
+      // Der Abstand des Messpunktes vom Mittelpunkt wird berechnet.
+      abstandMesspunktMittelpunkt = this.messpunkte[i].distance(mittelpunkt);
+
+      // Falls der Messpunkt mit dem Mittelpunkt identisch ist wird eine RuntimeException geworfen.
+      if (abstandMesspunktMittelpunkt == 0.0)
+         {
+         String fehlermeldung = "Der Algorithmus zur Lösung des Ausgleichsproblems konnte nicht ausgeführt werden." +
+            "\n\nUrsache: Während eines Iterationsschrittes ist der Messpunkt mit den Koordinaten " + 
+            this.messpunkte[i] + " mit dem momentanten Näherungswert für den Kreismittelpunkt identisch. Dadurch " +
+            "wird ein Element einer Jakobi-Matrix unendlich groß.";
+
+         Jakobimatrix.logger.severe("Der Messpunkt " + this.messpunkte[i] + " ist mit dem Kreismittelpunkt identisch.");
+
+         throw new RuntimeException(fehlermeldung);
+         }
+
       // Das Inverse des Abstands des Messpunkts vom Mittelpunkt wird berechnet.
-      inverserAbstandMesspunktMittelpunkt = 1.0 / this.messpunkte[i].distance(mittelpunkt);
-      
+      double inverserAbstandMesspunktMittelpunkt = 1.0 / abstandMesspunktMittelpunkt;
+
       // Die Elemente der Jakobi-Matrix werden initialisiert.
       jakobiMatrix[i][0] = inverserAbstandMesspunktMittelpunkt * (mittelpunkt.getX() - this.messpunkte[i].getX());
       jakobiMatrix[i][1] = inverserAbstandMesspunktMittelpunkt * (mittelpunkt.getY() - this.messpunkte[i].getY());
-      jakobiMatrix[i][2] = -1;
+      jakobiMatrix[i][2] = -1.0;
       }
-   
+
    return jakobiMatrix;
    }
-
 }
