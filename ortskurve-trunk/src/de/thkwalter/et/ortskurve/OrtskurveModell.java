33a46eea2f85043300e6c4a4c24b693e7acb38eb
th.k.walter@googlemail.com <th.k.walter@googlemail.com@08437e74-564d-73a1-201a-7a4970f44d9d>
2013-10-01 20:39:22 +0000

    Knopf "ESB-Berechnung" wird ausgeblendet, falls keine Lösung existiert.

--- ortskurve-trunk/src/de/thkwalter/et/ortskurve/OrtskurveModell.java
+++ ortskurve-trunk/src/de/thkwalter/et/ortskurve/OrtskurveModell.java
@@ -70,6 +70,11 @@ private final int xPixelGrafik = 540;
  */
 private final int yPixelGrafik = 270;
 
+/**
+ * Dieses Flag zeigt an, ob die Lösung des Ausgleichsproblems angezeigt werden soll. 
+ */
+private boolean loesungAnzeigen;
+
 // =====================================================================================================================
 // =====================================================================================================================
 
@@ -316,4 +321,33 @@ public Vector2D[] getMesspunkte()
    return this.messpunkte;
    }
 
+// =====================================================================================================================
+// =====================================================================================================================
+
+/**
+ * Diese Methode gibt zurück, ob die Ortskurve angezeigt werden soll.
+ * 
+ * @return <tt>true</tt>, falls die Ortskurve angezeigt werden soll; <tt>false</tt>, falls die Lösung der Ortskurve 
+ * nicht angezeigt werden soll.
+ */
+public boolean isLoesungAnzeigen()
+   {
+   // Es wird zurückgegeben, ob die Ortskurve angezeigt werden soll.
+   return this.loesungAnzeigen;
+   }
+
+// =====================================================================================================================
+// =====================================================================================================================
+
+/**
+ * Es wird im Datenmodell gespeichert, ob die Ortskurve angezeigt werden soll. 
+ * 
+ * @param loesungAnzeigen <tt>true</tt>, falls die Ortskurve angezeigt werden soll; <tt>false</tt>, falls die Lösung der 
+ * Ortskurve nicht angezeigt werden soll.
+ */
+public void setLoesungAnzeigen(boolean loesungAnzeigen)
+   {
+   // Es wird im Datenmodell gespeichert, ob die Ortskurve angezeigt werden soll.
+   this.loesungAnzeigen = loesungAnzeigen;
+   }
 }
