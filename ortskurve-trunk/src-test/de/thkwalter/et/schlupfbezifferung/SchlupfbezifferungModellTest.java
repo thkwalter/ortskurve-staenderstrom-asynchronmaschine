c69d1f05f591fe0279f9cb8dfe43efb66adb5116
th.k.walter@googlemail.com <th.k.walter@googlemail.com@08437e74-564d-73a1-201a-7a4970f44d9d>
2014-05-09 19:14:28 +0000

    Attribut ortskurve der Klasse SchlupfbezifferungModell implementiert.

---
+++ ortskurve-trunk/src-test/de/thkwalter/et/schlupfbezifferung/SchlupfbezifferungModellTest.java
@@ -0,0 +1,162 @@
+/**
+ *  Copyright 2014 Th. K. Walter, Nürnberg.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package de.thkwalter.et.schlupfbezifferung;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.lang.reflect.Field;
+
+import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
+import org.junit.Before;
+import org.junit.Test;
+
+import de.thkwalter.et.ortskurve.Ortskurve;
+
+/**
+ * Diese Klasse enthält Tests für die Klasse {@link SchlupfbezifferungModell}.
+ * 
+ * @author Th. K. Walter
+ */
+public class SchlupfbezifferungModellTest
+{
+/**
+ * Der Prüfling der Klasse {@link SchlupfbezifferungModell}.
+ */
+private SchlupfbezifferungModell schlupfbezifferungModell;
+
+// =====================================================================================================================
+// =====================================================================================================================
+
+/**
+ * Diese Methode initialisiert die Tests.
+ * 
+ * @throws java.lang.Exception
+ */
+@Before
+public void setUp() throws Exception
+   {
+   // Der Prüfling wird erzeugt.
+   this.schlupfbezifferungModell = new SchlupfbezifferungModell();
+   }
+
+// =====================================================================================================================
+// =====================================================================================================================
+
+/**
+ * Test des Konstruktors {@link SchlupfbezifferungModell#SchlupfbezifferungModell()}.
+ */
+@Test
+public void testSchlupfbezifferungModell()
+   {
+   // Es wird überprüft, ob der Prüfling erzeugt worden ist.
+   assertNotNull(this.schlupfbezifferungModell);
+   }
+
+// =====================================================================================================================
+// =====================================================================================================================
+
+/**
+ * Test der Methode {@link SchlupfbezifferungModell#getInversionszentrum()}.
+ * 
+ * @throws SecurityException 
+ * @throws NoSuchFieldException 
+ * @throws IllegalAccessException 
+ * @throws IllegalArgumentException 
+ */
+@Test
+public void testGetInversionzentrum() throws NoSuchFieldException, SecurityException, IllegalArgumentException, 
+   IllegalAccessException
+   {
+   // Das in diesem Test verwendete Inversionszentrum (in A) wird erzeugt.
+   Vector2D testInversionszentrum = new Vector2D(0.0, 0.0);
+   
+   // Das in diesem Test verwendete Inversionszentrum (in A) wird im Datenmodell gespeichert.
+   Field feldInversionszentrum = SchlupfbezifferungModell.class.getDeclaredField("inversionszentrum");
+   feldInversionszentrum.setAccessible(true);
+   feldInversionszentrum.set(this.schlupfbezifferungModell, testInversionszentrum);
+   
+   // Es wird überprüft, ob das Inversionszentrum (in A) korrekt zurückgegeben wird.
+   assertEquals(testInversionszentrum, this.schlupfbezifferungModell.getInversionszentrum());
+   }
+
+// =====================================================================================================================
+// =====================================================================================================================
+
+/**
+ * Test der Methode {@link SchlupfbezifferungModell#setInversionszentrum(Vector2D)}.
+ */
+@Test
+public void testSetInversionzentrum()
+   {
+   // Das in diesem Test verwendete Inversionszentrum (in A) wird erzeugt.
+   Vector2D testInversionszentrum = new Vector2D(0.0, 0.0);
+   
+   // Die zu testende Methode wird aufgerufen.
+   this.schlupfbezifferungModell.setInversionszentrum(testInversionszentrum);
+   
+   // Es wird überprüft, ob das Inversionszentrum (in A) korrekt im Datenmodell der Schlupfbezifferungsbestimmung
+   // gespeichert worden ist.
+   assertEquals(testInversionszentrum, this.schlupfbezifferungModell.getInversionszentrum());
+   }
+
+// =====================================================================================================================
+// =====================================================================================================================
+
+/**
+ * Test der Methode {@link SchlupfbezifferungModell#getOrtskurve()}.
+ * 
+ * @throws SecurityException 
+ * @throws NoSuchFieldException 
+ * @throws IllegalAccessException 
+ * @throws IllegalArgumentException 
+ */
+@Test
+public void testGetOrtskurve() throws NoSuchFieldException, SecurityException, IllegalArgumentException, 
+   IllegalAccessException
+   {
+   // Die in diesem Test verwendete Ortskurve wird erzeugt.
+   Ortskurve testOrtskurve = new Ortskurve(new Vector2D(0.0, 0.0), 1.0);
+   
+   // Das in diesem Test verwendete Ortskurve wird im Datenmodell gespeichert.
+   Field feldOrtskurve = SchlupfbezifferungModell.class.getDeclaredField("ortskurve");
+   feldOrtskurve.setAccessible(true);
+   feldOrtskurve.set(this.schlupfbezifferungModell, testOrtskurve);
+   
+   // Es wird überprüft, ob die Ortskurve korrekt zurückgegeben wird.
+   assertEquals(testOrtskurve, this.schlupfbezifferungModell.getOrtskurve());
+   }
+
+// =====================================================================================================================
+// =====================================================================================================================
+
+/**
+ * Test der Methode {@link SchlupfbezifferungModell#setOrtskurve(Ortskurve)}.
+ */
+@Test
+public void testSetOrtskurve()
+   {
+   // Die in diesem Test verwendete Ortskurve wird erzeugt.
+   Ortskurve testOrtskurve = new Ortskurve(new Vector2D(0.0, 0.0), 1.0);
+   
+   // Die zu testende Methode wird aufgerufen.
+   this.schlupfbezifferungModell.setOrtskurve(testOrtskurve);
+   
+   // Es wird überprüft, ob die Ortskurve korrekt im Datenmodell der Schlupfbezifferungsbestimmung gespeichert worden 
+   // ist.
+   assertEquals(testOrtskurve, this.schlupfbezifferungModell.getOrtskurve());
+   }
+}
---
+++ ortskurve-trunk/src/de/thkwalter/et/schlupfbezifferung/SchlupfbezifferungController.java
@@ -0,0 +1,56 @@
+/**
+ *  Copyright 2014 Th. K. Walter, Nürnberg.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package de.thkwalter.et.schlupfbezifferung;
+
+import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
+
+/**
+ * Dieser Klasse steuert die Bestimmung der Schlupfbezifferung.
+ * 
+ * @author Th. K. Walter
+ */
+public class SchlupfbezifferungController
+{
+/**
+ * Das Datenmodell der Schlupfbezifferungsbestimmung.
+ */
+private SchlupfbezifferungModell schlupfbezifferungModell;
+
+// =====================================================================================================================
+// =====================================================================================================================
+
+/**
+ * Diese Methode bestimmt die Schlupfbezifferung.
+ */
+private void schlupfbezifferungBestimmenIntern()
+   {
+   // Das Inversionszentrum (in A) wird berechnet und im Datenmodell der Schlupfbezifferungsbestimmung gespeichert.
+//   this.schlupfbezifferungModell.setInversionszentrum(this.inversionszentrumBerechnen());
+   }
+
+// =====================================================================================================================
+// =====================================================================================================================
+
+/**
+ * Diese Methode berechnet das Inversionszentrum (in A).
+ * 
+ * @return Das Inversionzentrum (in A)
+ */
+private Vector2D inversionszentrumBerechnen()
+   {
+   return null;
+   }
+}
