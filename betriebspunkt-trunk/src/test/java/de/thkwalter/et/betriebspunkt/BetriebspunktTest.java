ef7db62b754db0d9fccfb905d02978233401627e
th.k.walter@googlemail.com <th.k.walter@googlemail.com@08437e74-564d-73a1-201a-7a4970f44d9d>
2014-05-23 21:29:23 +0000

    Initial import.

---
+++ betriebspunkt-trunk/src/test/java/de/thkwalter/et/betriebspunkt/BetriebspunktTest.java
@@ -0,0 +1,61 @@
+/**
+ *  Copyright 2014 Th. K. Walter, Nürnberg.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package de.thkwalter.et.betriebspunkt;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Diese Klasse enthält Tests für die Klasse {@link Betriebspunkt}.
+ * 
+ * @author Th. K. Walter
+ */
+public class BetriebspunktTest
+{
+/**
+ * Ein Prüfling der Klasse {@link Betriebspunkt}.
+ */
+private Betriebspunkt betriebspunkt;
+
+// =====================================================================================================================
+// =====================================================================================================================
+
+/**
+ * Diese Methode initialisiert die Tests.
+ * 
+ * @throws java.lang.Exception
+ */
+@Before
+public void setUp() throws Exception
+   {
+   // Der Prüfling wird erzeugt.
+   this.betriebspunkt = new Betriebspunkt();
+   }
+
+// =====================================================================================================================
+// =====================================================================================================================
+
+/**
+ * Test des Konstruktors {@link Betriebspunkt#Betriebspunkt()}.
+ */
+@Test
+public void testBetriebspunkt()
+   {
+   // Es wird überprüft, ob der Prüfling erzeugt worden ist.
+   }
+}
