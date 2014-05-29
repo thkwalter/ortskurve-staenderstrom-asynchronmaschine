a6d57a58517e085d7b54979c340dae3f6902b028
th.k.walter@googlemail.com <th.k.walter@googlemail.com@08437e74-564d-73a1-201a-7a4970f44d9d>
2014-05-29 12:12:43 +0000

    Weiterentwicklung

--- betriebspunkt-trunk/src/main/java/de/thkwalter/et/betriebspunkt/Betriebspunkt.java
+++ betriebspunkt-trunk/src/main/java/de/thkwalter/et/betriebspunkt/Betriebspunkt.java
@@ -15,6 +15,8 @@
  */
 package de.thkwalter.et.betriebspunkt;
 
+import org.apache.commons.math3.complex.Complex;
+
 /**
  * Diese Klasse repräsentiert einen Betriebspunkt.
  * 
@@ -23,9 +25,36 @@ package de.thkwalter.et.betriebspunkt;
 public class Betriebspunkt
 {
 /**
- * Der Leiterstrom (in A).
+ * Der effektive Leiterstrom (in A).
  */
 private double i_L;
+
+/**
+ * Die effektive Leiter-Leiterspannung (in V).
+ */
+private double u_LL;
+
+/**
+ * Die elektrische Leistung (in kW)
+ */
+private double p_el;
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+/**
+ * Die Scheinleistung (in kVA).
+ */
+private double p_s;
+
+/**
+ * Die Phasenverschiebung zwischen Strangstrom und Strangspannung.
+ */
+private double cosPhi;
+
+/**
+ * Der komplexe Zeiger des Ständerstroms (in A)
+ */
+private Complex z_i_s;
 
 // =====================================================================================================================
 // =====================================================================================================================
@@ -33,25 +62,119 @@ private double i_L;
 /**
  * Der Konstruktor initialisiert den Betriebspunkt
  * 
- * @param i_l Der Leiterstrom (in A)
+ * @param i_l Der effektive Leiterstrom (in A)
+ * @param u_LL Die effektive Leiter-Leiterspannung (in V)
+ * @param p_el Die elektrische Leistung (in kW)
+ * @param schaltungstyp Der Schaltungstyp
  */
-public Betriebspunkt(double i_L)
+public Betriebspunkt(double i_L, double u_LL, double p_el, Schaltungstyp schaltungstyp)
    {
    // Die Attribute werden initialisiert.
    this.i_L = i_L;
+   this.u_LL = u_LL;
+   this.p_el = p_el;
+   
+   // Die Scheinleistung (in kVA) wird berechnet.
+   this.p_s = Math.sqrt(3.0) * this.u_LL() * this.i_L() / 1000.0;
+   
+   // Die Phasenverschiebung zwischen Strangstrom und Strangspannung wird berechnet.
+   this.cosPhi = this.p_el / this.p_s;
+   
+   // Ein vom Schaltungstyp abhängiger Faktor wird berechnet.
+   double faktor = schaltungstyp == Schaltungstyp.STERN ? 1.0 : 1.0 / Math.sqrt(3.0);
+   
+   // Der Realteil des Strangstroms (in A) wird berechnet.
+   double i_s_re = faktor * this.i_L * this.cosPhi;
+   
+   // Der Imaginärteil des Strangstroms (in A) wird berechnet. Dabei ist zu beachten, dass für elektrische Maschinen der
+   // Phasenwinkel phi negativ ist.
+   double i_s_im = faktor * this.i_L * Math.sin(- Math.acos(this.cosPhi));
+   
+   // Der komplexe Zeiger des Ständerstroms (in A) wird erzeugt.
+   this.z_i_s = new Complex(i_s_re, i_s_im);
    }
 
 // =====================================================================================================================
 // =====================================================================================================================
 
 /**
- * Diese Methode gibt den Leiterstrom (in A) zurück.
+ * Diese Methode gibt den effektiven Leiterstrom (in A) zurück.
  * 
- * @return Der Leiterstrom (in A)
+ * @return Der effektive Leiterstrom (in A)
  */
 public double i_L()
    {
-   // Der Leiterstrom (in A) wird zurückgegeben.
+   // Der effektive Leiterstrom (in A) wird zurückgegeben.
    return this.i_L;
+   }
+
+// =====================================================================================================================
+// =====================================================================================================================
+
+/**
+ * Diese Methode gibt die effektive Leiter-Leiterspannung (in V) zurück.
+ * 
+ * @return Die effektive Leiter-Leiterspannung (in V)
+ */
+public double u_LL()
+   {
+   // Die effektive Leiter-Leiterspannung (in V) wird zurückgegeben.
+   return this.u_LL;
+   }
+
+// =====================================================================================================================
+// =====================================================================================================================
+
+/**
+ * Diese Methode gibt die elektische Leistung (in kW) zurück.
+ * 
+ * @return Die elektrische Leistung (in kW)
+ */
+public double p_el()
+   {
+   // Die elektrische Leistung (in kW) wird zurückgegeben.
+   return this.p_el;
+   }
+
+// =====================================================================================================================
+// =====================================================================================================================
+
+/**
+ * Diese Methode gibt die Scheinleistung (in kVA) zurück.
+ * 
+ * @return Die Scheinleistung (in kVA)
+ */
+public double p_s()
+   {
+   // Die Scheinleistung (in kVA) wird zurückgegeben.
+   return this.p_s;
+   }
+
+// =====================================================================================================================
+// =====================================================================================================================
+
+/**
+ * Diese Methode gibt die Phasenverschiebung zwischen Strangstrom und Strangspannung zurück.
+ * 
+ * @return Die Phasenverschiebung zwischen Strangstrom und Strangspannung wird zurückgegeben.
+ */
+public double cosPhi()
+   {
+   // Die Phasenverschiebung zwischen Strangstrom und Strangspannung wird zurückgegeben.
+   return this.cosPhi;
+   }
+
+// =====================================================================================================================
+// =====================================================================================================================
+
+/**
+ * Diese Methode gibt den komplexen Zeiger des Strangstroms (in A) zurück.
+ * 
+ * @return Der komplexe Zeiger des Strangstroms (in A)
+ */
+public Complex z_i_s()
+   {
+   // Der komplexe Zeiger des Strangstroms (in A) wird zurückgegeben
+   return this.z_i_s;
    }
 }
--- betriebspunkt-trunk/src/test/java/de/thkwalter/et/betriebspunkt/BetriebspunktTest.java
+++ betriebspunkt-trunk/src/test/java/de/thkwalter/et/betriebspunkt/BetriebspunktTest.java
@@ -20,6 +20,7 @@ import static org.junit.Assert.assertNotNull;
 
 import java.lang.reflect.Field;
 
+import org.apache.commons.math3.complex.Complex;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -47,7 +48,7 @@ private Betriebspunkt betriebspunkt;
 public void setUp() throws Exception
    {
    // Der Prüfling wird erzeugt.
-   this.betriebspunkt = new Betriebspunkt(3.2546);
+   this.betriebspunkt = new Betriebspunkt(3.2546, 396.98, 1.9259, Schaltungstyp.STERN);
    }
 
 // =====================================================================================================================
@@ -62,8 +63,24 @@ public void testBetriebspunkt()
    // Es wird überprüft, ob der Prüfling erzeugt worden ist.
    assertNotNull(this.betriebspunkt);
    
-   // Es wird überprüft, ob der Leiterstrom (in A) korrekt initialisiert worden ist.
+   // Es wird überprüft, ob der effektive Leiterstrom (in A) korrekt initialisiert worden ist.
    assertEquals(3.2546, this.betriebspunkt.i_L(), 0.0);
+   
+   // Es wird überprüft, ob die effektive Leiter-Leiterspannung (in A) korrekt zurückgegeben wird.
+   assertEquals(396.98, this.betriebspunkt.u_LL(), 0.0);
+   
+   // Es wird überprüft, ob die elektrische Leistung (in kW) korrekt zurückgegeben wird.
+   assertEquals(1.9259, this.betriebspunkt.p_el(), 0.0);
+   
+   // Es wird überprüft, ob die Scheinleistung (in kVA) korrekt zurückgegeben wird.
+   assertEquals(2.238, this.betriebspunkt.p_s(), 2.238 / 1000.0);
+   
+   // Es wird überprüft, ob die Phasenverschiebung zwischen Strangstrom und Strangspannung korrekt zurückgegeben wird.
+   assertEquals(0.8606, this.betriebspunkt.cosPhi(), 0.8606 / 1000.0);
+   
+   // Es wird überprüft, ob der komplexe Zeiger des Strangstroms (in A) korrekt zurückgegeben wird.
+   assertEquals(2.801, this.betriebspunkt.z_i_s().getReal(), 2.801 / 1000.0);
+   assertEquals(-1.657, this.betriebspunkt.z_i_s().getImaginary(), 1.657 / 1000.0);
    }
 
 // =====================================================================================================================
@@ -80,12 +97,133 @@ public void testBetriebspunkt()
 @Test
 public void test_i_L() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException
    {
-   // Der in diesem Test verwendete Leiterstrom (in A) wird im Prüfling gespeichert.
-   Field betriebspunktFeld = Betriebspunkt.class.getDeclaredField("i_L");
-   betriebspunktFeld.setAccessible(true);
-   betriebspunktFeld.setDouble(this.betriebspunkt, 2.8769);
+   // Der in diesem Test verwendete effektive Leiterstrom (in A) wird im Prüfling gespeichert.
+   Field i_L_Feld = Betriebspunkt.class.getDeclaredField("i_L");
+   i_L_Feld.setAccessible(true);
+   i_L_Feld.setDouble(this.betriebspunkt, 2.8769);
    
-   // Es wird überprüft, ob der Leiterstrom (in A) korrekt zurückgegeben wird.
+   // Es wird überprüft, ob der effektive Leiterstrom (in A) korrekt zurückgegeben wird.
    assertEquals(2.8769, this.betriebspunkt.i_L(), 0.0);
+   }
+
+// =====================================================================================================================
+// =====================================================================================================================
+
+/**
+ * Test der Methode {@link Betriebspunkt#u_LL()}.
+ * 
+ * @throws SecurityException 
+ * @throws NoSuchFieldException 
+ * @throws IllegalAccessException 
+ * @throws IllegalArgumentException 
+ */
+@Test
+public void test_u_LL() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException
+   {
+   // Die in diesem Test verwendete effektive Leiter-Leiterspannung (in V) wird im Prüfling gespeichert.
+   Field u_LL_Feld = Betriebspunkt.class.getDeclaredField("u_LL");
+   u_LL_Feld.setAccessible(true);
+   u_LL_Feld.setDouble(this.betriebspunkt, 397.72);
+   
+   // Es wird überprüft, ob die effektive Leiter-Leiterspannung (in A) korrekt zurückgegeben wird.
+   assertEquals(397.72, this.betriebspunkt.u_LL(), 0.0);
+   }
+
+// =====================================================================================================================
+// =====================================================================================================================
+
+/**
+ * Test der Methode {@link Betriebspunkt#p_el()}.
+ * 
+ * @throws SecurityException 
+ * @throws NoSuchFieldException 
+ * @throws IllegalAccessException 
+ * @throws IllegalArgumentException 
+ */
+@Test
+public void test_p_el() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException
+   {
+   // Die in diesem Test verwendete elektrische Leistung (in kW) wird im Prüfling gespeichert.
+   Field p_el_Feld = Betriebspunkt.class.getDeclaredField("p_el");
+   p_el_Feld.setAccessible(true);
+   p_el_Feld.setDouble(this.betriebspunkt, 1.6462);
+   
+   // Es wird überprüft, ob die elektrische Leistung (in kW) korrekt zurückgegeben wird.
+   assertEquals(1.6462, this.betriebspunkt.p_el(), 0.0);
+   }
+
+// =====================================================================================================================
+// =====================================================================================================================
+
+/**
+ * Test der Methode {@link Betriebspunkt#p_s()}.
+ * 
+ * @throws SecurityException 
+ * @throws NoSuchFieldException 
+ * @throws IllegalAccessException 
+ * @throws IllegalArgumentException 
+ */
+@Test
+public void test_p_s() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException
+   {
+   // Die in diesem Test verwendete Scheinleistung (in kVA) wird im Prüfling gespeichert.
+   Field p_s_Feld = Betriebspunkt.class.getDeclaredField("p_s");
+   p_s_Feld.setAccessible(true);
+   p_s_Feld.setDouble(this.betriebspunkt, 1.9820);
+   
+   // Es wird überprüft, ob die Scheinleistung (in kVA) korrekt zurückgegeben wird.
+   assertEquals(1.9820, this.betriebspunkt.p_s(), 0.0);
+   }
+
+// =====================================================================================================================
+// =====================================================================================================================
+
+/**
+ * Test der Methode {@link Betriebspunkt#cosPhi()}.
+ * 
+ * @throws SecurityException 
+ * @throws NoSuchFieldException 
+ * @throws IllegalAccessException 
+ * @throws IllegalArgumentException 
+ */
+@Test
+public void test_cosPhi() throws NoSuchFieldException, SecurityException, IllegalArgumentException, 
+   IllegalAccessException
+   {
+   // Die in diesem Test verwendete Phasenverschiebung zwischen Strangstrom und Strangspannung wird im Prüfling 
+   // gespeichert.
+   Field cosPhi_Feld = Betriebspunkt.class.getDeclaredField("cosPhi");
+   cosPhi_Feld.setAccessible(true);
+   cosPhi_Feld.setDouble(this.betriebspunkt, 0.83062);
+   
+   // Es wird überprüft, ob die Phasenverschiebung zwischen Strangstrom und Strangspannung korrekt zurückgegeben wird.
+   assertEquals(0.83062, this.betriebspunkt.cosPhi(), 0.0);
+   }
+
+// =====================================================================================================================
+// =====================================================================================================================
+
+/**
+ * Test der Methode {@link Betriebspunkt#z_i_s()}.
+ * 
+ * @throws SecurityException 
+ * @throws NoSuchFieldException 
+ * @throws IllegalAccessException 
+ * @throws IllegalArgumentException 
+ */
+@Test
+public void test_z_i_s() throws NoSuchFieldException, SecurityException, IllegalArgumentException, 
+   IllegalAccessException
+   {
+   // Der in diesem Test verwendete komplexe Zeiger des Strangstroms (in A) wird erzeugt.
+   Complex test_z_i_s = new Complex(2.3897, -1.6020);
+   
+   // Der in diesem Test verwendete komplexe Zeiger des Strangstroms (in A) wird im Prüfling gespeichert.
+   Field z_i_s_Feld = Betriebspunkt.class.getDeclaredField("z_i_s");
+   z_i_s_Feld.setAccessible(true);
+   z_i_s_Feld.set(this.betriebspunkt, test_z_i_s);
+   
+   // Es wird überprüft, ob der komplexe Zeiger des Strangstroms (in A) korrekt zurückgegeben wird.
+   assertEquals(test_z_i_s, this.betriebspunkt.z_i_s());
    }
 }
