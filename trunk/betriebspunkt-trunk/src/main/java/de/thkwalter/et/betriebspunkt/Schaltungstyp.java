a61bd7aa32455c70a4bf4e4382040c3c85ae8088
th.k.walter@googlemail.com <th.k.walter@googlemail.com@08437e74-564d-73a1-201a-7a4970f44d9d>
2014-05-29 12:12:43 +0000

    Weiterentwicklung

---
+++ trunk/betriebspunkt-trunk/src/main/java/de/thkwalter/et/betriebspunkt/Schaltungstyp.java
@@ -0,0 +1,34 @@
+/**
+ *  Copyright 2014 Th. K. Walter, Nürnberg.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package de.thkwalter.et.betriebspunkt;
+
+/**
+ * Diese KLasse repräsentiert den Schaltungstyp.
+ * 
+ * @author Th. K. Walter
+ */
+public enum Schaltungstyp
+{
+/**
+ * Diese Konstante zeigt an, dass die elektrische Maschine im Dreieck geschalten ist. 
+ */
+DREIECK,
+
+/**
+ * Diese Konstante zeigt an, dass die elektrische Maschine im Stern geschalten ist. 
+ */
+STERN;
+}
